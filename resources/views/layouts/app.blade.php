e1a5e4836d347615c1d9f6116686a0c9c18fdc43
AhmerAftabPatel <ahmerpatel123@gmail.com>
2024-03-26 03:08:29 -0500

    UI change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,20 +17,25 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
-
+            @include('layouts.sidebar')
             <!-- Page Heading -->
-            @if (isset($header))
+            
+
+            <!-- Page Content -->
+            <div class="sm:ml-64">
+   <div class="rounded-lg dark:border-gray-700">
+   @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
             @endif
-
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+        </div>
+        </div>
         </div>
     </body>
 </html>
